04620a3d6607a62dd4e3b541f07739c9d82d1f3b
Tiago Lopes <tiagompglopes@gmail.com>
2018-03-03 16:32:39 +0000

    Remove counter redux example

--- src/connectors/redux/reducers/index.js
+++ src/connectors/redux/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
-import counter from './counter';
 import menu from './menu';
 import drawer from './drawer';
 
 const reducer = combineReducers({
-  counter,
   menu,
   drawer,
 });
