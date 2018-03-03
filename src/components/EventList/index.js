3a74652d8b4f4d1060b2a3cbe255eace42cd9382
Tiago Lopes <tiagompglopes@gmail.com>
2018-03-03 22:50:41 +0000

    Visual improvements

--- src/components/EventList/index.js
+++ src/components/EventList/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withRouteData } from 'react-static';
+import { withRouteData, Link } from 'react-static';
 import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
 // Icons
-import DateRangeIcon from 'material-ui-icons/DateRange';
+import ScheduleIcon from 'material-ui-icons/Schedule';
+import PlaceIcon from 'material-ui-icons/Place';
 import FriendlyRangeDate from './FriendlyRangeDate';
 
 const styles = theme => ({
@@ -15,10 +16,28 @@ const styles = theme => ({
     paddingBottom: 16,
     marginTop: theme.spacing.unit * 2,
   }),
+  headline: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+  },
+  link: {
+    textDecoration: 'none',
+    color: '#039be5',
+  },
+  timeandplace: {
+    marginBottom: theme.spacing.unit * 3,
+  },
   icons: {
     verticalAlign: 'text-bottom',
+    width: 20,
+    height: 20,
+  },
+  location: {
+    paddingLeft: theme.spacing.unit * 2,
   },
 });
+
+const googleMapLink = url => `https://www.google.pt/maps/place/${encodeURI(url)}`;
 
 class EventList extends React.Component {
   constructor(props) {
@@ -31,21 +50,45 @@ class EventList extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <Grid container spacing={16}>
-        {this.state.events.map(event => (
-          <Grid key={event.uniqId} item xs={12}>
-            <Paper className={classes.paperEvent}>
-              <Typography variant="headline" component="h2" paragraph>
-                {event.name}
-              </Typography>
-              <Typography variant="subheading">
-                <DateRangeIcon className={classes.icons} />
-                <FriendlyRangeDate startDate={event.startDate} endDate={event.endDate} />
-              </Typography>
-            </Paper>
-          </Grid>
-        ))}
-      </Grid>
+      <div>
+        <Typography paragraph variant="headline" component="h1" className={classes.headline}>
+          Upcoming events
+        </Typography>
+        <Grid container spacing={16}>
+          {this.state.events.map(event => (
+            <Grid key={event.uniqId} item xs={12}>
+              <Paper className={classes.paperEvent}>
+                <Typography variant="title" component="h2" paragraph>
+                  <Link to={event.url} href={event.url} className={classes.link}>
+                    {event.name}
+                  </Link>
+                </Typography>
+                <Typography variant="subheading" className={classes.timeandplace}>
+                  <ScheduleIcon className={classes.icons} />
+                  { ' ' }
+                  <FriendlyRangeDate startDate={event.startDate} endDate={event.endDate} />
+                  { event.location && (
+                    <span className={classes.location}>
+                      <PlaceIcon className={classes.icons} />
+                      { ' ' }
+                      <Link
+                        to={googleMapLink(event.location.address)}
+                        href={googleMapLink(event.location.address)}
+                        className={classes.link}
+                      >
+                        { event.location.name }
+                      </Link>
+                    </span>
+                  )}
+                </Typography>
+                <Typography variant="body1" paragraph gutterBottom>
+                  {event.description}
+                </Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     );
   }
 }
