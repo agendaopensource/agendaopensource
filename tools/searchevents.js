29729c1a73169e45451fef7220437cfbb58afb0d
Tiago Lopes <tiagompglopes@gmail.com>
2018-03-14 00:52:09 +0000

    Add missing files

---
+++ tools/searchevents.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+const moment = require('moment');
+
+dotenv.config();
+
+const key = process.env.MEETUP_KEY;
+const meetupGroups = process.env.MEETUP_GROUPS.split(',');
+
+async function getMeetupGroupEvents(groupName) {
+  return data;
+}
+
+meetupGroups.map(async (groupName) => {
+  const { data: events } = await axios.get(`https://api.meetup.com/${groupName}/events?&sign=true&key=${key}`);
+
+  events.map((event) => {
+    const startDate =
+      moment(`${event.local_date} ${event.local_time}`)
+        .format('YYYY-MM-DD kk:mm');
+
+    const endDate =
+      moment(startDate)
+        .add(event.duration / 1000, 'second') // If not there can be assumed 3h
+        .format('YYYY-MM-DD kk:mm');
+
+    const eventParsed = {
+      '@context': 'http://schema.org',
+      '@type': 'Event',
+      location: {
+        '@type': 'Place',
+        name: event.venue.name,
+        address: event.venue.address_1,
+        district: event.venue.city,
+      },
+      name: event.name,
+      startDate,
+      endDate,
+      description: event.description.replace(/<[^>]+>/g, ''),
+      url: event.link,
+    };
+
+    process.stdout.write(`${JSON.stringify(eventParsed, null, 4)},\n`);
+    return eventParsed;
+  });
+});
