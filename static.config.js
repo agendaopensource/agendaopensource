4c8443e1b993394f329e3f4749c7fced8aeaed9b
Tiago Lopes <tiagompglopes@gmail.com>
2018-03-28 10:46:58 +0100

    Add manifest file

--- static.config.js
+++ static.config.js
@@ -84,6 +84,7 @@ export default {
         <title>Open Agenda</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <Body>
         {children}
