5d644e1a46ffa19b65d162942cda15a1a963382b
Tiago Lopes <tiagompglopes@gmail.com>
2018-03-28 05:18:24 +0100

    Improve footer and styles update

--- static.config.js
+++ static.config.js
@@ -63,21 +63,6 @@ export default {
         component: 'src/containers/About',
       },
       {
-        path: '/blog',
-        component: 'src/containers/Blog',
-        getData: () => ({
-          posts,
-          events: tailoredEvents,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          component: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
-      {
         is404: true,
         component: 'src/containers/404',
       },
