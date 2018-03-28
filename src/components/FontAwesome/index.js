0f98717c3d20099af3522bcb088358a8c21b7205
Tiago Lopes <tiagompglopes@gmail.com>
2018-03-28 06:11:34 +0100

    Removed fontawesome tests

--- src/components/FontAwesome/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import fontAwesome from 'font-awesome/css/font-awesome.css';
-
-const FontAwesome = ({iconName}) => (
-  <i class={`fa ${iconName}`}></i>
-);
-
-export default FontAwesome;
